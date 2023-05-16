30bdb614cc547150e44fcf55453efe84d0f3325a
Ravi Kumar <kumar.ravi.vegan@gmail.com>
2023-05-16 13:54:26 +0530

    Complete User Roles|Authorization

--- 12TUT/config/allowedOrigins.js
+++ 12TUT/config/allowedOrigins.js
@@ -3,5 +3,4 @@ const allowedOrigins = [
     "http://127.0.0.1:5500",
     "http://localhost:3500",
 ];
-
 module.exports = allowedOrigins;
--- 12TUT/config/corsOptions.js
+++ 12TUT/config/corsOptions.js
@@ -1,11 +1,10 @@
-const allowedOrigins = require("./allowedOrigins");
-
+const whiteList = require("./allowedOrigins");
 const corsOptions = {
     origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        if (whiteList.indexOf(origin) !== -1 || !origin) {
             callback(null, true);
         } else {
-            callback(new Error("Not allowed by CORS"));
+            callback(new Error("Not allowed due to CORS"));
         }
     },
     optionsSuccessStatus: 200,
