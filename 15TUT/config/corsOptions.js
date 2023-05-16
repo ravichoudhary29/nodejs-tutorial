6a58aaa975f970d3378a84f59eef4316b4288031
Ravi Kumar <kumar.ravi.vegan@gmail.com>
2023-05-16 20:46:36 +0530

    Complete async CRUD Operations

---
+++ 15TUT/config/corsOptions.js
@@ -0,0 +1,13 @@
+const whiteList = require("./allowedOrigins");
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whiteList.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed due to CORS"));
+        }
+    },
+    optionsSuccessStatus: 200,
+};
+
+module.exports = corsOptions;
